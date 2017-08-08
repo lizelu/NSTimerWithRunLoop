1ebc495df32f9c0ecf74340abe5f2111b09cd56a
Mr.LuDashi <lizelusdut@qq.com>
2017-08-08 11:44:55 +0800

    update source code

--- NSTimerWithRunLoop/ThirdTableViewCell.swift
+++ NSTimerWithRunLoop/ThirdTableViewCell.swift
@@ -13,18 +13,15 @@ class ThirdTableViewCell: UITableViewCell {
     @IBOutlet var timeLabel: UILabel!
     
     override func awakeFromNib() {
-        super.awakeFromNib()
         let queue: DispatchQueue = DispatchQueue.global()
         let source = DispatchSource.makeTimerSource(flags: DispatchSource.TimerFlags(rawValue: 0), queue: queue)
-        
         let timer = UInt64(1) * NSEC_PER_SEC
+        
         source.scheduleRepeating(deadline: DispatchTime.init(uptimeNanoseconds: UInt64(timer)), interval: DispatchTimeInterval.seconds(Int(1)), leeway: DispatchTimeInterval.seconds(0))
         
-        let timeout = 1    //倒计时时间
-        
+        let timeout = 1
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm:ss"
-        //设置要处理的事件, 在我们上面创建的queue队列中进行执行
         source.setEventHandler {
             if(timeout < 0) {
                 source.cancel()
@@ -34,15 +31,10 @@ class ThirdTableViewCell: UITableViewCell {
                 self.timeLabel.text = "\(dateFormatter.string(from: Date()))"
             }
         }
-        
         source.resume()
-
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
